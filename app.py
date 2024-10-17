856bb6f10c6db329604a49bac07eefeb314196ef
springsteenptrck <springsteenptrck@gmail.com>
2024-10-17 13:52:53 -0400

    Changed debug parameter to false. Deleted unused configurations.

--- app.py
+++ app.py
@@ -11,13 +11,7 @@ app = Flask(__name__)
 
 # Configuration
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False  # Explicitly set SSL to False
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')  # Set default sender
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -45,4 +39,4 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
